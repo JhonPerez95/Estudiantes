d36e46b6833c0be6f2a366f5f36d7dfd591b67a6
jhonperez95 <jhonperez.j95@gmail.com>
2021-10-26 21:31:40 -0500

    Atrapando datos al usuario

--- src/Estudiantes.java
+++ src/Estudiantes.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 public class Estudiantes {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Para solicitar informacion al usuario
+        Scanner scUI = new Scanner(System.in);
+
+        // Creamos variables necesarias
+        int opcion = 0;
+        String profession;
+        boolean flag = false;
+
+        // Creamos el string mensaje a mostrar
+        String messageOption = "REGISTRO DE ESTUDIANTES:\n" + "1. Registro de estudiantes\n"
+                + "2. Consulta Datos Por Estudiantes\n" + "3. Consulta Carreras\n" + "4. Salir\n";
+
+        // TODO:
+        // TRAER LA CLASE DE DATOS
+
+        do {
+            try {
+                // Atrapamos la opcion que el user digite
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null, messageOption, "________________",
+                        JOptionPane.QUESTION_MESSAGE));
+
+                System.out.println("Opcion elegida por el usuario: " + opcion);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "ERROR ! " + e.getMessage());
+            }
+        } while (opcion != 4);
+
     }
 }
